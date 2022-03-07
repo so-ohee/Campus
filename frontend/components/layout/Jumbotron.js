c94c0c18b5c302b40dc79e7b395d0bdfd1afa7b8
aff4110 <aff4110@gmail.com>
2022-03-07 11:37:07 +0900

    Feat: 점보트론 컴포넌트 작성

---
+++ frontend/components/layout/Jumbotron.js
@@ -0,0 +1,10 @@
+function Jumbotron() {
+  return (
+    <>
+      <h1>자이언트점보트론</h1>
+      <h1>자이안쓰점보트론</h1>
+    </>
+  );
+}
+
+export default Jumbotron;
--- frontend/pages/Layout.js
+++ frontend/pages/Layout.js
@@ -1,10 +1,12 @@
 import Navbar from "/components/layout/Navbar";
 import Footer from "/components/layout/Footer";
+import Jumbotron from "/components/layout/Jumbotron";
 
 function Layout({ children }) {
   return (
     <>
       <Navbar />
+      <Jumbotron />
       {children}
       <Footer />
     </>
