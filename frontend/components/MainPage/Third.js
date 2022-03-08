d05dd5ebc9d0b33ee58ba64655d9c61df4a2686a
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-08 13:40:12 +0900

    FIX First, Second, Third

--- frontend/components/MainPage/Third.js
+++ frontend/components/MainPage/Third.js
@@ -1,7 +1,12 @@
+import { Container } from 'react-bootstrap';
+import styles from "/styles/MainPageThird.module.css";
+
 function Third() {
     return (
-        <div>
-            세번째 페이지
+        <div className={styles.third_main}>
+            <Container>
+                <h1 className={styles.third_h1}>리뷰로 보는 캠핑장 TOP 3</h1>
+            </Container>
         </div>
     );
 }
