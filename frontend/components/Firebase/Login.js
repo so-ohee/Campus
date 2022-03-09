5db7261ba75dba1fb84c887e766f932f43162398
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-09 21:24:12 +0900

    FIX firebase (firebase 연결 오류)

--- frontend/components/Firebase/Login.js
+++ frontend/components/Firebase/Login.js
@@ -1,7 +1,26 @@
 import { Button, Modal } from 'react-bootstrap';
 import styles from '/styles/Firebase/Login.module.css';
+import {
+    createUserWithEmailAndPassword,
+    signInWithEmailAndPassword,
+    signInWithPopup,
+    GoogleAuthProvider,
+    GithubAuthProvider,
+} from "@firebase/auth";
+import { authService, firebaseInstance  } from "firebase";
 
 function Login(props) {
+    const onSocialClick = async (event) => {
+        const name = event.target.name;
+        let provider;
+        if (name === "google") {
+        provider = new GoogleAuthProvider();
+        } else if (name === "github") {
+        provider = new GithubAuthProvider();
+        }
+        const data = await signInWithPopup(authService, provider);
+    };
+
     return (
         <Modal
             {...props}
@@ -10,16 +29,19 @@ function Login(props) {
             centered
         >
             <Modal.Header closeButton>
-                <Modal.Title id="contained-modal-title-vcenter">
-                    Login
+                <Modal.Title className={styles.login_header} id="contained-modal-title-vcenter">
+                    <div>
+                        <h1 style={{fontWeight: "bold"}}>Login</h1>
+                    </div>
                 </Modal.Title>
             </Modal.Header>
             <Modal.Body>
                 <div className={styles.login_pic_div}>
                     <img className={styles.login_pic} src="/logo.png" />
                 </div>
-                <div>
-                    
+                <div className={styles.login_pic_div}>
+                    <img className={styles.login_kakao_pic} name="kakao" onClick={onSocialClick} src="/kakaologin.png" />
+                    <img className={styles.login_google_pic} name="google" onClick={onSocialClick}  src="/googlelogin.png" />
                 </div>
             </Modal.Body>
             <Modal.Footer>
--- frontend/components/Firebase/firebase.js
+++ frontend/components/Firebase/firebase.js
@@ -1,9 +1,8 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import * as firebase from "firebase/app";
+import 'firebase/auth';
+import 'firebase/storage';
+import 'firebase/firestore';
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
     apiKey: process.env.NEXT_PUBLIC_API_Key,
     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
@@ -14,5 +13,8 @@ const firebaseConfig = {
     appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
+// const app = initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+export const authService = firebase.auth();
