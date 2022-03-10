fcfce49d7be8b65cb306394f20f87fe2975a2b13
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-10 17:31:39 +0900

    ADD CampingUse.js

--- frontend/pages/campingplace.js
+++ frontend/pages/campingplace.js
@@ -2,6 +2,7 @@ import CampingExplain from '/components/CampingPlace/CampingExplain.js';
 import CampingIntro from '/components/CampingPlace/CampingIntro.js';
 import CampingMap from '/components/CampingPlace/CampingMap.js';
 import CampingReview from '/components/CampingPlace/CampingReview.js';
+import CampingUse from '/components/CampingPlace/CampingUse.js';
 
 function campingplace() {
   return (
@@ -9,7 +10,8 @@ function campingplace() {
       <CampingExplain />
       {/* <CampingIntro /> */}
       {/* <CampingMap /> */}
-      <CampingReview />
+      {/* <CampingReview /> */}
+      <CampingUse />
     </div>
   );
 }
