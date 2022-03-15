64b3c2d758701651daa53d81ac1f4bb7fa0468de
서은민 <sem6169@naver.com>
2022-03-15 20:40:35 +0900

    Feat: 메인페이지 추천(계절)

--- backend/spring/src/main/java/com/ssafy/camping/repository/CampingRepository.java
+++ backend/spring/src/main/java/com/ssafy/camping/repository/CampingRepository.java
@@ -2,6 +2,13 @@ package com.ssafy.camping.repository;
 
 import com.ssafy.camping.entity.Camping;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CampingRepository extends JpaRepository<Camping, Integer> {
+
+    @Query(value = "SELECT * FROM camping WHERE(thema_envrn_cl REGEXP :thema) order by blog_cnt desc limit 50", nativeQuery = true)
+    List<Camping> searchByThema(@Param("thema") String thema);
 }
