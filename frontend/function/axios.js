a3526a14bea07effe6e053f2bdc9a0dd9e1d3034
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-15 10:31:12 +0900

    회원 Uid DB 전송

--- frontend/function/axios.js
+++ frontend/function/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
+const url = "http://localhost:8080/";
+
 export const sendUserUid = async (userUid) => {
-    await axios.get(
-        
-    )
-}
+    await axios.get(`${ url }`+ `/` + `user` +`/`+ `${userUid}`)
+};
