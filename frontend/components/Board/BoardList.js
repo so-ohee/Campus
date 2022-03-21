431a84007c9260dc22d1a28a858bcdbc5324d9ce
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-21 11:24:36 +0900

    FIX boardList (테이블 수정)

--- frontend/components/Board/BoardList.js
+++ frontend/components/Board/BoardList.js
@@ -52,85 +52,82 @@ function boardlist(props) {
 
     return (
         <div>
-            <Container>
+            <Container >
                 <h1 className={styles.boardlist_h1}>게시판</h1>
-                <div className={styles.boardlist_button}>
-                    <Row style={{ justifyContent: "right" }}>
-                        <div className={styles.boardlist_title_content}>
-                            <h6>제목/내용</h6>
-                        </div>
-                        <Form.Group style={{width:"300px", float: "right"}} controlId="formBasicPassword">
-                            <Form.Control type="password" placeholder="Password" />
-                        </Form.Group>
-                        <Button variant="success" style={{width: "100px"}} onClick={submitSign}>리뷰 작성</Button>
-                    </Row>
-                </div>
 
-                <div style={{marginBottom: "-1.2%", marginLeft: "4%"}}>
-                    <Tabs style={{width: "500px"}} defaultActiveKey="profile" id="uncontrolled-tab-example" className="mb-3">
-                        <Tab eventKey="전체" title="전체">
-                            
-                        </Tab>
-                        <Tab eventKey="Q&A" title="Q&A">
-                            
-                        </Tab>
-                        <Tab eventKey="자유" title="자유">
-                            
-                        </Tab>
-                        <Tab eventKey="후기" title="후기">
-                            
-                        </Tab>
-                        <Tab eventKey="뉴스" title="뉴스">
-                            
-                        </Tab>
-                    </Tabs>
-                </div>
+                <Row style={{ justifyContent: "right" }}>
+                    <div className={styles.boardlist_title_content}>
+                        <h6>제목/내용</h6>
+                    </div>
+                    <Form.Group style={{width:"300px", float: "right"}} controlId="formBasicPassword">
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Button variant="success" style={{width: "100px"}} onClick={submitSign}>리뷰 작성</Button>
+                </Row>
 
-                <div className={styles.boardlist_table}>
-                    <table>
-                        <thead>
-                            <tr className={styles.boardlist_thead_tr}>
-                                <th style={{width: "100px", textAlignLast: "center"}}>번호</th>
-                                <th style={{width: "100px", textAlignLast: "center"}}>카테고리</th>
-                                <th style={{width: "700px", textAlignLast: "center"}}>제목</th>
-                                <th style={{width: "120px", textAlignLast: "center"}}>작성자</th>
-                                <th style={{width: "120px", textAlignLast: "center"}}>작성일</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            {
-                                dummy.map((element, index) => {
-                                    return (
-                                        <>
-                                            {
-                                                element.category === "리뷰" && 
-                                                    <tr className={styles.boardlist_tbody_tr} onClick={submitSign2}>
-                                                        <td style={{ width: "100px", textAlignLast: "center" }}>{index + 1}</td>
-                                                        <td style={{ width: "150px", textAlignLast: "center" }}>{element.category}</td>
-                                                        <td style={{ width: "720px", paddingLeft: "3%" }}>{element.content}</td>
-                                                        <td style={{ width: "120px", textAlignLast: "center" }}>{element.writer}</td>
-                                                        <td style={{ width: "150px", textAlignLast: "center" }}>{element.date}</td>
-                                                    </tr>
-                                            }
-                                            {
-                                                element.category !== "리뷰" && 
-                                                    <tr className={styles.boardlist_tbody_tr} onClick={submitSign3}>
-                                                        <td style={{ width: "100px", textAlignLast: "center" }}>{index + 1}</td>
-                                                        <td style={{ width: "150px", textAlignLast: "center" }}>{element.category}</td>
-                                                        <td style={{ width: "720px", paddingLeft: "3%" }}>{element.content}</td>
-                                                        <td style={{ width: "120px", textAlignLast: "center" }}>{element.writer}</td>
-                                                        <td style={{ width: "150px", textAlignLast: "center" }}>{element.date}</td>
-                                                    </tr>
-                                            }
-                                        </>
-                                    )
-                                })
-                            }
-                        </tbody>
-                    </table>
-                </div>
-                <Pagination className={styles.boardlist_pagination}>{items}</Pagination>
+                <Tabs style={{width: "500px"}} defaultActiveKey="전체" id="uncontrolled-tab-example" className="mb-3">
+                    <Tab eventKey="전체" title="전체">
+                        
+                    </Tab>
+                    <Tab eventKey="Q&A" title="Q&A">
+                        
+                    </Tab>
+                    <Tab eventKey="자유" title="자유">
+                        
+                    </Tab>
+                    <Tab eventKey="후기" title="후기">
+                        
+                    </Tab>
+                    <Tab eventKey="뉴스" title="뉴스">
+                        
+                    </Tab>
+                </Tabs>
             </Container>
+
+            <Container style={{height: "700px", marginTop: "-0.7%"}}>
+                <table className={styles.boardlist_table}>
+                    <thead>
+                        <tr className={styles.boardlist_thead_tr}>
+                            <th style={{width: "100px", textAlignLast: "center"}}>번호</th>
+                            <th style={{width: "100px", textAlignLast: "center"}}>카테고리</th>
+                            <th style={{width: "700px", textAlignLast: "center"}}>제목</th>
+                            <th style={{width: "120px", textAlignLast: "center"}}>작성자</th>
+                            <th style={{width: "120px", textAlignLast: "center"}}>작성일</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {
+                            dummy.map((element, index) => {
+                                return (
+                                    <>
+                                        {
+                                            element.category === "리뷰" && 
+                                                <tr className={styles.boardlist_tbody_tr} onClick={submitSign2}>
+                                                    <td style={{ width: "100px", textAlignLast: "center" }}>{index + 1}</td>
+                                                    <td style={{ width: "150px", textAlignLast: "center" }}>{element.category}</td>
+                                                    <td style={{ width: "720px", paddingLeft: "3%" }}>{element.content}</td>
+                                                    <td style={{ width: "120px", textAlignLast: "center" }}>{element.writer}</td>
+                                                    <td style={{ width: "150px", textAlignLast: "center" }}>{element.date}</td>
+                                                </tr>
+                                        }
+                                        {
+                                            element.category !== "리뷰" && 
+                                                <tr className={styles.boardlist_tbody_tr} onClick={submitSign3}>
+                                                    <td style={{ width: "100px", textAlignLast: "center" }}>{index + 1}</td>
+                                                    <td style={{ width: "150px", textAlignLast: "center" }}>{element.category}</td>
+                                                    <td style={{ width: "720px", paddingLeft: "3%" }}>{element.content}</td>
+                                                    <td style={{ width: "120px", textAlignLast: "center" }}>{element.writer}</td>
+                                                    <td style={{ width: "150px", textAlignLast: "center" }}>{element.date}</td>
+                                                </tr>
+                                        }
+                                    </>
+                                )
+                            })
+                        }
+                    </tbody>
+                </table>
+            </Container>
+            <Pagination className={styles.boardlist_pagination}>{items}</Pagination>
         </div>
     );
 }
