a41e23f7b4b3656302b6526219f5faa80d61b95d
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-21 23:53:38 +0900

    FIX 불필요 코드 삭제 (메인페이지 부분)

--- frontend/components/MainPage/First.js
+++ frontend/components/MainPage/First.js
@@ -4,25 +4,6 @@ import CampingCard from "/components/common/CampingCard";
 import { viewCamping } from "../../function/axios";
 import { useEffect, useState } from 'react';
 
-// const dummy = [
-//     {
-//         title: "달천공원오토캠핑장",
-//         address: "강원도 횡성군 갑천면 외갑천로 301",
-//         hashtag: ["일출명소", "일몰명소", "봄꽃여행", "여름물놀이", "걷기길"],
-//     },
-//     {
-//         title: "청풍호오토캠핑장",
-//         address: "충청북도 제천시 청풍면 용곡길 211번길 2",
-//         hashtag: ["짚라인", "체험형", "캠핑요리"],
-//     },
-//     {
-//         title: "마음이 머무는 곳",
-//         address: "강원도 화천군 사내면 포화로 653-76",
-//         hashtag: ["아로마향초만들기", "천연염색", "캠핑요리"],
-//     },
-// ];
-
-
 function First() {
 
     const [title, setTitle] = useState("");
@@ -35,9 +16,6 @@ function First() {
                 setCampingplace(response.data.seasonList);
     });
     }, []);
-
-    // console.log(campingplace);
-    // console.log();
     
     return (
         <div className={styles.first_main}>
