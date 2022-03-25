af804ad6138cc363b043a3c38b7967194fa41ad3
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-25 15:20:37 +0900

    FIX frontend 배포 오류 수정

---
+++ frontend/page/_document.js
@@ -0,0 +1,25 @@
+import Document, { Head, Main, NextScript } from 'next/document';
+import React from 'react';
+
+export default class MyDocument extends Document {
+    render() {
+        return (
+        <html>
+            <Head>
+            <meta
+                name="viewport"
+                content="width=device-width, initial-scale=1, shrink-to-fit=no"
+            />
+            // 여기에 폰트 임포팅
+            // 여기에 공통 CSS e.g.reset-css/common.css
+            </Head>
+            <body>
+            <div id="root">
+                <Main />
+                <NextScript />
+            </div>
+            </body>
+        </html>
+        );
+    }
+}
