991392a448fdab4dc3ecfc07b52091a0b7ead5cd
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-25 15:00:08 +0900

    FIX frontend 배포 오류 수정

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -5,21 +5,23 @@ import Layout from "./Layout";
 import "/styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Script from "next/script"
-import { useState } from 'react';
 
 function MyApp({ Component, pageProps }) {
 
   return (
     <>
       <Head>
+        <meta http-equiv="Cache-Control" content="no-cache, no-store, must-revalidate" />
+        <meta http-equiv="Pragma" content="no-cache" />
+        <base href="/"/>
         <meta charSet="UTF-8" />
         <title>Campus</title>
         <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
 
       <Script
-        src="//dapi.kakao.com/v2/maps/sdk.js?appkey=6aa07b0f5d18874d44c830b57f8392a0&libraries=services,clusterer&autoload=false"
-        type="text/babel"
+        src="//dapi.kakao.co m/v2/maps/sdk.js?appkey=6aa07b0f5d18874d44c830b57f8392a0&libraries=services,clusterer&autoload=false"
+        type="text/javascript"
       />
 
       <Layout>
