b495bdea2454ef3677bc290f09ccebf3400be94b
alex0317@naver.com <joohanpark1513@gmail.com>
2022-03-25 13:17:16 +0900

    FIX frontend 오류 수정

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,6 +1,6 @@
-import First from '/components/MainPage/First.js';
-import Second from '/components/MainPage/Second.js';
-import Third from '/components/MainPage/Third.js';
+import First from '../components/MainPage/First.js';
+import Second from '../components/MainPage/Second.js';
+import Third from '../components/MainPage/Third.js';
 import "firebase/compat/auth";
 import "firebase/compat/firestore";
 import "firebase/compat/storage";
