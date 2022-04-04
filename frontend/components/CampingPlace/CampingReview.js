ba2c5adb1b766900a50b2e7bf634597305a1bf07
이제민 <jemmm12@gmail.com>
2022-04-04 15:03:53 +0900

    FIX 블로그 리뷰 css

--- frontend/components/CampingPlace/CampingReview.js
+++ frontend/components/CampingPlace/CampingReview.js
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { Container, Col, Row } from "react-bootstrap";
+import { Container, Col, Row, Pagination } from "react-bootstrap";
 import styles from "../../styles/CampingPlace/CampingReview.module.css";
 import { viewBoard } from "../../function/axios";
 import axios from "axios"
@@ -13,16 +13,17 @@ function CampingReview(props) {
     const [blogs, setBlogs] = useState([])
     const sigungu = props.props.sigunguNm
     const campingName = props.props.facltNm
+    const [blogPage, setBlogPage] = useState(1)
 
     useEffect(() => {
         viewBoard(props.props.campingId).then((res) => setDummy(res.data.board));
         axios({
             method: 'get',
-            url: `https://dapi.kakao.com/v2/search/blog?query=${sigungu} ${campingName}&size=5`,
+            url: `https://dapi.kakao.com/v2/search/blog?query=${sigungu} ${campingName}&size=10`,
             headers: { Authorization: 'KakaoAK 755938934fdfd53eecb5a27918ac35e9' },
         })
             .then((res) => {
-            console.log(res);
+            // console.log(res);
             setBlogs(res.data.documents)
             })
             .catch((err) => {
@@ -68,22 +69,39 @@ function CampingReview(props) {
                 </div>
                 
             </Container>
-
+            <h2 className="fw-bold">블로그 리뷰</h2>
+            <hr></hr>
             <div>
-                {blogs.map((element, index) => {
+                {blogs.slice(blogPage*5-5,blogPage*5).map((element, index) => {
                     return (
-                        <div key={index} onClick={() => window.open(element.url, '_blank')} style={{cursor:"pointer"}}>
-                            <img  src={element.thumbnail} />
-                            <div >
-                                {element.title.replace(/(<([^>]+)>)/ig,"").replace(/&#34;/ig,'"')}
+                        <div key={index}>
+                            <div className="d-flex ">
+                                <div  style={{ width: "130px", height:"130px"}} >
+                                <img  src={element.thumbnail} onClick={() => window.open(element.url, '_blank')} style={{cursor:"pointer"}}/>
+                                </div>
+                                <div className='ms-1 mt-1'>
+                                    <h5 className="fw-bold" onClick={() => window.open(element.url, '_blank')} style={{cursor:"pointer"}}>
+                                        {element.title.replace(/(<([^>]+)>)/ig,"").replace(/&#34;/ig,'"')}
+                                    </h5>
+                                    {element.contents.replace(/(<([^>]+)>)/ig,"").replace(/&#34;/ig,'"')}
+                                    <h5 className="mt-2 mb-0">{element.datetime.slice(0,10)}</h5>
+                                </div>
                             </div>
-                            {element.contents.replace(/(<([^>]+)>)/ig,"").replace(/&#34;/ig,'"')}
-                            <h5>{element.datetime.slice(0,10)}</h5>
+                        <hr></hr>
                         </div>
                     );
                 })}
             </div>
-
+            <Pagination style={{justifyContent: "center"}}>
+                <Pagination.Item
+                    active={blogPage === 1}
+                    onClick={()=> setBlogPage(1)}
+                >1</Pagination.Item>
+                <Pagination.Item
+                    active={blogPage === 2}
+                    onClick={()=> setBlogPage(2)}
+                >2</Pagination.Item>
+            </Pagination>
         </>
     );
 }
