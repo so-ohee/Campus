8aebc7f94cdb4ebaa83281ff8b47b0df8e32ba22
sohee960328 <sohee960328@gmail.com>
2022-04-07 18:46:07 +0900

    Fix: 회원 탈퇴 시 작성한 댓글 삭제

--- backend/spring/src/main/java/com/ssafy/camping/repository/CommentRepository.java
+++ backend/spring/src/main/java/com/ssafy/camping/repository/CommentRepository.java
@@ -5,8 +5,12 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
     void deleteByBoardId(Integer boardId);
 
     Page<Comment> findByBoardId(Integer boardId, Pageable pageable);
+
+    List<Comment> findByUserUid(String userUid);
 }
